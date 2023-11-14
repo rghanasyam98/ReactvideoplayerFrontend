7afd41d8d862ecbd5cddd24682721bac1fe8b225
Ghanasyam Raveendran <rghanasyam9@gmail.com>
2023-11-14 12:09:06 +0530

    commit after updated base url

--- src/services/serviceUrl.js
+++ src/services/serviceUrl.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-export const baseurl='http://localhost:5000';
+// export const baseurl='http://localhost:5000';
+export const baseurl='https://mediaplayerbackend-hgh3.onrender.com';
+
 
 // const instance=axios.create({
 //     baseURL:baseurl
 // })
 
 // export default instance;
+
